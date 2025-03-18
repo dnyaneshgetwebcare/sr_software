7b15a15eef26be790f59cf1b155c18e0790c51c0
Dnyaneshwar <dnyaneshwareh@gmail.com>
2025-03-19 02:46:56 +0530

    update

--- frontend/views/site/not_found.php
+++ frontend/views/site/not_found.php
@@ -2,7 +2,7 @@
 <div class="container d-flex justify-content-center align-items-center">
 				<div class="page-inner" style="margin-top: 200px">
 					<div class="d-flex flex-column align-items-center">
-						<img src="../../img/logo2.jpg" width="250" alt="Page Not Found">
+						<img src="../../img/logo2.JPG" width="250" alt="Page Not Found">
 						<h2 class="h1 mt-4 mb-4 fw-bold">Sorry! page not found.</h2>
 						<p class="text-center op-7 mb-5 h5">Invalid URL please check and try again!<br></p>
 
