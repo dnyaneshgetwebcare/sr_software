3869f2a810b9dbac48621fab6403b61caf213d99
desk014 <dnyaneshwareh@gmail.com>
2025-03-31 01:44:03 +0530

    API

--- backend/controllers/ApiController.php
+++ backend/controllers/ApiController.php
@@ -12,7 +12,7 @@ use yii\web\Response;
 
 class ApiController extends \yii\web\Controller
 {
-      public function behaviors()
+    public function behaviors()
     {
         $behaviors = parent::behaviors();
 
@@ -20,7 +20,7 @@ class ApiController extends \yii\web\Controller
         $behaviors['corsFilter'] = [
             'class' => Cors::class,
             'cors' => [
-                'Origin' => ['*'], // Allow all domains
+                'Origin' => ['http://localhost'], // Allow all domains
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
                 'Access-Control-Allow-Credentials' => true, // Allow credentials (cookies, auth)
                 'Access-Control-Allow-Headers' => ['Authorization', 'Content-Type', 'X-Requested-With'],
@@ -35,57 +35,57 @@ class ApiController extends \yii\web\Controller
         return $this->render('index');
     }
 
-  public function actionItemList()
-  {
-    Yii::$app->response->format = Response::FORMAT_JSON;
-    $type = isset($_GET['type'])?$_GET['type'] : '';
-    $category = isset($_GET['category'])?$_GET['category'] : '';
-    $page_limt = isset($_GET['page_limit'])?$_GET['page_limit'] : 20;
-    $page_nos = isset($_GET['page_nos'])?$_GET['page_nos'] : 1;
-    $offset = ($page_limt * $page_nos) -$page_limt;
-    $item_master = ItemMaster::find()->select(['item_master.id', 'item_master.name', 'item_master.details',  'type_id', 'type_master.name as type_name', 'item_master.category_id', 'category_master.name as category_name', 'rent_amount', 'colour_cat', 'color_master.name as color_name', 'images', 'size'])
-      ->leftJoin('type_master','type_master.id = item_master.type_id')
-      ->leftJoin('category_master',
-        'item_master.category_id = category_master.id')
-      ->leftJoin('color_master', 'colour_cat = color_master.id')
-      ->andWhere(['scrab_status' =>
-        'No' , 'delete_status' => 0 , 'skip_website' =>
-        0])->andFilterWhere(['item_master.category_id' => $category, 'type_id'=> $type])->limit($page_limt)
-      ->offset($offset)->asArray
-      ()->all();
-    $image_def_path =\Yii::$app->request->BaseUrl.'https://app.thesoyara.com/uploads/';
-    $no_image_path = \Yii::$app->request->BaseUrl.'https://app.thesoyara.com/img/no-image.jpg';
+    public function actionItemList()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $type = isset($_GET['type'])?$_GET['type'] : '';
+        $category = isset($_GET['category'])?$_GET['category'] : '';
+        $page_limt = isset($_GET['page_limit'])?$_GET['page_limit'] : 20;
+        $page_nos = isset($_GET['page_nos'])?$_GET['page_nos'] : 1;
+        $offset = ($page_limt * $page_nos) -$page_limt;
+        $item_master = ItemMaster::find()->select(['item_master.id', 'item_master.name', 'item_master.details',  'type_id', 'type_master.name as type_name', 'item_master.category_id', 'category_master.name as category_name', 'rent_amount', 'colour_cat', 'color_master.name as color_name', 'images', 'size'])
+            ->leftJoin('type_master','type_master.id = item_master.type_id')
+            ->leftJoin('category_master',
+                'item_master.category_id = category_master.id')
+            ->leftJoin('color_master', 'colour_cat = color_master.id')
+            ->andWhere(['scrab_status' =>
+                'No' , 'delete_status' => 0 , 'skip_website' =>
+                0])->andFilterWhere(['item_master.category_id' => $category, 'type_id'=> $type])->limit($page_limt)
+            ->offset($offset)->asArray
+            ()->all();
+        $image_def_path =\Yii::$app->request->BaseUrl.'https://app.thesoyara.com/uploads/';
+        $no_image_path = \Yii::$app->request->BaseUrl.'https://app.thesoyara.com/img/no-image.jpg';
 
-    return array("item_master" => $item_master,'no_image_path' => $no_image_path, 'image_def_path' =>
-      $image_def_path );
+        return array("item_master" => $item_master,'no_image_path' => $no_image_path, 'image_def_path' =>
+            $image_def_path );
 
     }
     public function actionTypeList()
-  {
-    Yii::$app->response->format = Response::FORMAT_JSON;
-    $type = isset($_GET['type'])?$_GET['type'] : '';
-    $category_id = isset($_GET['category_id'])?$_GET['category_id'] : '';
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $type = isset($_GET['type'])?$_GET['type'] : '';
+        $category_id = isset($_GET['category_id'])?$_GET['category_id'] : '';
 
-    $type_master = TypeMaster::find()->andFilterWhere(['id' => $type, 'category_id' =>$category_id])->andWhere(['dispaly_main_site'
-    =>
-      '1' ]) ->asArray()->all();
-    return $type_master;
+        $type_master = TypeMaster::find()->andFilterWhere(['id' => $type, 'category_id' =>$category_id])->andWhere(['dispaly_main_site'
+        =>
+            '1' ]) ->asArray()->all();
+        return $type_master;
 
-  }
+    }
 
     public function actionCategoryList()
-  {
-    Yii::$app->response->format = Response::FORMAT_JSON;
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
 
-    $category_id = isset($_GET['category_id'])?$_GET['category_id'] : '';
+        $category_id = isset($_GET['category_id'])?$_GET['category_id'] : '';
 
-    $category_master = CategoryMaster::find()->andFilterWhere(['id' => $category_id])->andWhere(['dispaly_main_site'
-    =>
-      '1' ]) ->asArray()->all();
-    return $category_master;
+        $category_master = CategoryMaster::find()->andFilterWhere(['id' => $category_id])->andWhere(['dispaly_main_site'
+        =>
+            '1' ]) ->asArray()->all();
+        return $category_master;
 
-  }
- public function actionImageView($filename)
+    }
+    public function actionImageView($filename)
     {
         // Secure folder where images are stored
         $filePath = Yii::getAlias('@web/uploads/2/') . $filename;
