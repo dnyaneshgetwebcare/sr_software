c5f4172a95856f699d09a08fcb3d1d88ea30873e
desk014 <dnyaneshwareh@gmail.com>
2025-05-18 17:38:45 +0530

    packing

--- backend/controllers/PackingController.php
+++ backend/controllers/PackingController.php
@@ -5,9 +5,11 @@ namespace backend\controllers;
 use backend\models\BookingHeader;
 use backend\models\BookingItem;
 use backend\models\CustomerMaster;
+
 use Yii;
 use yii\db\Query;
 use yii\helpers\ArrayHelper;
+use yii\web\Response;
 
 class PackingController extends \yii\web\Controller
 {
@@ -62,14 +64,31 @@ public function actionBookingSearch($q = null, $id = null) {
 
   function actionGetPackingItem()
   {
+    if(Yii::$app->request->isAjax) {
+       Yii::$app->response->format = Response::FORMAT_JSON;
+      $_selected_items = isset($_POST['selected_item']) ? $_POST['selected_item'] :null;
+      if ($_selected_items == null) {
+        return array("error" => "Please select items for packing", 'flag' => false);
+      }
+      $_selected_boooking_ids_all = isset($_POST['selected_header']) ? $_POST['selected_header'] : array();
+      foreach ($_selected_boooking_ids_all as $_selected_boooking_id_header) {
+        BookingItem::updateAll(['packing_status' => 'Packed'], ['booking_id' => $_selected_boooking_id_header]);
+        unset($_selected_items[$_selected_boooking_id_header]);
+      }
+      foreach ($_selected_items as $selected_booking_id => $selected_item) {
 
-    $booking_ids = isset($_POST['booking_id_filter'])?$_POST['booking_id_filter'] : null;
-    $customer_ids = isset($_POST['customer_filter'])?$_POST['customer_filter'] : null;
-    $pickup_date = isset($_POST['filter_pickup_date'])?Yii::$app->helpercomponent->dateFormat($_POST['filter_pickup_date']) : null;
+        BookingItem::updateAll(['packing_status' => 'Packed'], ['booking_id' => $selected_booking_id, 'item_no' => $selected_item]);
+      }
+       return array('flag' => true);
+    }
+    $booking_ids = isset($_POST['booking_id_filter'])?$_POST['booking_id_filter'] : '';
+    $customer_ids = isset($_POST['customer_filter'])?$_POST['customer_filter'] : '';
+    $post_pickup_date = isset($_POST['filter_pickup_date'])?$_POST['filter_pickup_date'] : '';
+    $pickup_date = Yii::$app->helpercomponent->dateFormat($post_pickup_date);
 
     //if($booking_ids != null || $customer_ids!= null || $pickup_date!= null){
-      $booking_details = BookingItem::find()->select(['booking_header.booking_id', 'booking_header.pickup_date', 'booking_header.remark', 'customer_master.name as customer_name', 'item_master.name as description', 'booking_item.item_no', 'note', 'images'])->leftJoin('booking_header', 'booking_header.booking_id= booking_item.booking_id')->leftJoin('customer_master', 'customer_master.id = booking_header.customer_id')->where(['booking_item.item_status'=>'Booked', 'booking_header.order_status' => 'Open'])->leftJoin('item_master','item_master.id = booking_item.product_id')->andFilterWhere(['booking_header.booking_id' => $booking_ids, 'customer_id'=> $customer_ids, 'booking_header.pickup_date' => $pickup_date ])->createCommand()->queryAll();
-        return $this->render('packing_items',['booking_details'=> $booking_details]);
+      $booking_details = BookingItem::find()->select(['booking_header.booking_id', 'booking_header.pickup_date', 'booking_header.remark', 'customer_master.name as customer_name', 'item_master.name as description', 'booking_item.item_no', 'note', 'images'])->leftJoin('booking_header', 'booking_header.booking_id= booking_item.booking_id')->leftJoin('customer_master', 'customer_master.id = booking_header.customer_id')->where(['booking_item.item_status'=>'Booked', 'booking_header.order_status' => 'Open', 'booking_item.packing_status'=> 'Unpacked'])->leftJoin('item_master','item_master.id = booking_item.product_id')->andFilterWhere(['booking_header.booking_id' => $booking_ids, 'customer_id'=> $customer_ids, 'booking_header.pickup_date' => $pickup_date ])->createCommand()->queryAll();
+        return $this->render('packing_items',['booking_details'=> $booking_details, 'booking_ids'=>$booking_ids, 'customer_ids'=>$customer_ids,'post_pickup_date' => $post_pickup_date]);
    // }
 
 
--- backend/views/packing/packing_items.php
+++ backend/views/packing/packing_items.php
@@ -1,63 +1,178 @@
 <?php
 $this->title = 'Packing';
+    use kartik\form\ActiveForm;
+    use yii\helpers\Html;
 ?>
-
-<div>
-<table class="table table-head-bg-primary mt-4">
-										<thead>
-											<tr>
-												<th scope="col">#</th>
-												<th scope="col">Image</th>
-												<th scope="col">Details</th>
-												<th scope="col">Measurment</th>
-												<th scope="col">Notes</th>
-											</tr>
-										</thead>
-										<tbody>
-                    <?php
-
-                    use yii\helpers\Html;
-
-                    $cur_customer = null;
-                    $cur_booking_id = null;
-
-                    foreach ($booking_details as $booking_detail) {
-                      $img_url = Yii::$app->helpercomponent->getImageurl($booking_detail['images']);
-                      if($cur_booking_id != $booking_detail['booking_id']){
-                        $cur_booking_id = $booking_detail['booking_id'];
-                      ?>
-                      <tr>
-                        <td>
-                          <input type="checkbox"  name="selected_header[]" value="<?=
-                          $booking_detail['booking_id'];
-                        ?>" class="<?= 'all_'.$booking_detail['booking_id']; ?> check">
-                        </td>
-                        <td><?= date('d-m-Y', strtotime($booking_detail['pickup_date'])); ?></td>
-                        <td><?= $booking_detail['customer_name']; ?></td>
-                        <td colspan="2"><?= $booking_detail['remark']; ?></td>
-                      </tr>
-                        <?php } ?>
-                      <tr>
-                        <td><input type="checkbox"  name="selected_item['<?= $booking_detail['booking_id'];
-                        ?>'][]"
-                                   value="<?= $booking_detail['booking_id'];
-                        ?>" class="<?= 'all_'.$booking_detail['item_no']; ?> check"></td>
-                        <td <a class="image-popup-vertical-fit" href="<?= $img_url ?>"> <?= Html::img
-                          ($img_url, ['width' => '100', 'height' => '80']) ?> </a></td>
-                        <td><?= $booking_detail['description']; ?></td>
-                        <td><?= $booking_detail['note']; ?></td>
-                        <td></td>
-                      </tr>
+<style>
+    .customer_details {
+        background-color: #ffe767 !important;
+    }
+</style>
+<div class="image-gallery">
+<?php   $form = ActiveForm::begin(['id'=>'packing_submit', 'method' => 'post','action' =>
+    ['packing/get-packing-item'],'enableClientValidation'=>false]); ?>
+  <table class="table table-head-bg-primary mt-4 mb-5">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Image</th>
+      <th scope="col">Details</th>
+      <th scope="col">Measurement</th>
+      <th scope="col">Notes</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
 
 
-                    <?php
-                    } ?>
 
-										</tbody>
-									</table>
-<button type="button" class="btn btn-info" id="submit_pickup()" style="" >Submit</button>
+    $cur_customer = null;
+    $cur_booking_id = null;
+
+    foreach ($booking_details as $booking_detail) {
+      $img_url = Yii::$app->helpercomponent->getImageurl($booking_detail['images']);
+      if ($cur_booking_id != $booking_detail['booking_id']) {
+        $cur_booking_id = $booking_detail['booking_id'];
+        ?>
+        <tr>
+          <td class="customer_details">
+            <input type="checkbox" name="selected_header[]" value="<?= $booking_detail['booking_id']; ?>"
+                   class="order_check <?= 'all_' . $booking_detail['booking_id']; ?> check">
+          </td>
+          <td class="customer_details" style="font-size: large; font-weight: bold"><?= date('d-m-Y',
+              strtotime
+              ($booking_detail['pickup_date']));
+            ?></td>
+          <td class="customer_details" onclick="open_booking('index.php?r=booking/update&id=<?=
+            $booking_detail['booking_id']; ?>')" style="font-size: large; font-weight: bold; cursor: pointer"> <?= "(#"
+            .$booking_detail['booking_id'].") - ".$booking_detail['customer_name']; ?> </td>
+          <td colspan="2" class="customer_details"><?= $booking_detail['remark']; ?></td>
+        </tr>
+      <?php } ?>
+      <tr>
+        <td style="padding: 30px !important;">
+          <input type="checkbox" name="selected_item[<?= $booking_detail['booking_id']; ?>][]"
+                 value="<?= $booking_detail['item_no']; ?>" data-booking-id="<?= $booking_detail['booking_id']; ?>"
+                 class="<?= 'items_check item_check_' . $booking_detail['booking_id']; ?>  check"></td>
+        <td><a class="image-popup-vertical-fit" href="<?= $img_url ?>"> <?= Html::img
+            ($img_url, ['width' => '100', 'height' => '80']) ?> </a></td>
+        <td><?= $booking_detail['description']; ?></td>
+        <td><?= $booking_detail['note']; ?></td>
+        <td></td>
+      </tr>
+      <?php
+    } ?>
+
+    </tbody>
+  </table>
+<button type="button" class="btn btn-info" onclick="submitpacking()" style="position: fixed; bottom: 5px ">Submit</button>
+   <?php ActiveForm::end(); ?>
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-<script></script>
+<?php   $form = ActiveForm::begin(['id'=>'packing_search', 'method' => 'post','action' =>
+    ['packing/get-packing-item'],'enableClientValidation'=>false]); ?>
+<div style="display: none">
+  <input name = "filter_pickup_date" value ="<?= $post_pickup_date; ?>">
+  <input name = "customer_filter" value ="<?= json_encode($customer_ids) ?>">
+  <input name = "booking_id_filter" value ="<?= json_encode($booking_ids) ?>">
+
+</div>
+<?php ActiveForm::end(); ?>
+
+<script src="kai-admin-assets/js/core/jquery-3.7.1.min.js"></script>
+
+<script src="kai-admin-assets/js/plugin/jquery.magnific-popup/jquery.magnific-popup.min.js"></script>
+<script>
+  $('.image-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    removalDelay: 300,
+    gallery: {
+      enabled: true,
+    },
+    mainClass: 'mfp-with-zoom',
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function (openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+  });
+  let suppressEvent = false;
+  $(document).ready(function () {
+    $('.items_check').on('ifChanged', function (event) {
+      if (suppressEvent) return;
+      // Triggered when checkbox is changed (either checked or unchecked)
+      var booking_id = $(this).data('booking-id');
+      var all_status = $(`.item_check_${booking_id}`).filter(':checked').length === $(`.item_check_${booking_id}`).length;
+      suppressEvent = true;
+      if (all_status) {
+        $(`.all_${booking_id}`).iCheck('check');
+      } else {
+        $(`.all_${booking_id}`).iCheck('uncheck');
+      }
+      suppressEvent = false;
+    });
+
+    $('.order_check').on('ifChecked', function (event) {
+      if (suppressEvent) return;
+      // Triggered when checkbox is changed (either checked or unchecked)
+      var booking_id = $(this).val();
+      /* var all_status = $(`item_check_${booking_id}`).filter(':checked').length === $(`item_check_${booking_id}`).length;*/
+
+      suppressEvent = true;
+      $(`.item_check_${booking_id}`).iCheck('check');
+      suppressEvent = false;
+      /*} else {
+        $(`.item_check_${booking_id}`).iCheck('uncheck');
+      }*/
+    });
+    $('.order_check').on('ifUnchecked', function (event) {
+      if (suppressEvent) return;
+      // Triggered when checkbox is changed (either checked or unchecked)
+      var booking_id = $(this).val();
+      /* var all_status = $(`item_check_${booking_id}`).filter(':checked').length === $(`item_check_${booking_id}`).length;*/
+
+      suppressEvent = true;
+      $(`.item_check_${booking_id}`).iCheck('uncheck');
+      suppressEvent = false;
+      /*} else {
+        $(`.item_check_${booking_id}`).iCheck('uncheck');
+      }*/
+    });
+  });
+function open_booking(req_url){
+   window.open(req_url, '_blank');
+}
+
+function submitpacking() {
+ var url = $("#packing_submit").attr('action');    // Get form action URL
+    var data = $("#packing_submit").serialize();
+  $.ajax({
+        url: url,
+        type: 'post',
+        data : data,
+      beforeSend: function () {
+        $(".overlay").show();
+      },
+      complete: function () {
+        $(".overlay").hide();
+      },
+        success: function (data) {
+          if(data['flag']){
+            $("#packing_search").submit();
+          }else{
+            alert("Please select item");
+          }
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+          if(errorThrown=='Forbidden'){
+            alert(you_dont_have_accsess_label);
+          }
+        }
+      });
+}
+</script>
 
 
